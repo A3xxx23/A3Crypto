ab53eb0d8c6ceee20ac3908aee7f08ae1f2363e3
Angel Aquino <aa23-0272@unphu.edu.do>
2025-05-13 01:30:11 -0400

    change background color of Hero component to light blue

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -72,41 +72,37 @@ export const Table = ({ searchTerm }: Props) => {
 
 
     return (
-            <table className="mt-12 w-full max-w-xl mx-auto border-white/5 bg-white/10 rounded-xl shadow-lg">
-                <thead>
-                    <tr className="text-center">
-                        <th className="px-4 py-2 border-b">Cryptocurrency</th>
-                        <th className="px-4 py-2 border-b">Price</th>
-                        <th className="px-4 py-2 border-b">Market Cap</th>
-                        <th className="px-4 py-2 border-b">24h Change</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {filteredCoins.slice(0,10).map((coin) => (
-                        <tr key={coin.id} className="text-white text-center hover:bg-white/15">
-                        <td className="px-4 py-2 flex items-center text-center gap-2">
-                            <img src={coin.image} alt={coin.name} className="w-6 h-6" />
-                            <span className="overflow-hidden whitespace-nowrap text-ellipsis max-w-[150px]">{coin.name} ({coin.symbol.toUpperCase()})</span>
-                        </td>
-                        <td className="px-4 py-2">
-                            ${numeral(coin.current_price).format("0,0.00")}
-                        </td>
-                        <td className="px-4 py-2">
-                            ${numeral(coin.market_cap).format("0.00a")}
-                        </td>
-                        <td
-                            className={`px-4 py-2 ${
-                            coin.price_change_percentage_24h >= 0
-                            ? "text-green-400"
-                            : "text-red-400"
-                            }`}
-                        >
-                            {coin.price_change_percentage_24h?.toFixed(2)}%
-                        </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+        <table className="mt-12 w-full max-w-xl mx-auto border border-white/10 bg-white/3 rounded-lg overflow-hidden transition-shadow duration-300">
+    <thead className=" rounded-t-lg">
+        <tr className="text-center">
+            <th className="px-4 py-2">Cryptocurrency</th>
+            <th className="px-4 py-2">Price</th>
+            <th className="px-4 py-2">Market Cap</th>
+            <th className="px-4 py-2">24h Change</th>
+        </tr>
+    </thead>
+    <tbody>
+        {filteredCoins.slice(0, 10).map((coin) => (
+            <tr key={coin.id} className="text-white text-center hover:bg-white/15">
+                <td className="px-4 py-2 flex items-center text-center gap-2">
+                    <img src={coin.image} alt={coin.name} className="w-6 h-6" />
+                    <span className="overflow-hidden whitespace-nowrap text-ellipsis max-w-[150px]">{coin.name} ({coin.symbol.toUpperCase()})</span>
+                </td>
+                <td className="px-4 py-2">
+                    ${numeral(coin.current_price).format("0,0.00")}
+                </td>
+                <td className="px-4 py-2">
+                    ${numeral(coin.market_cap).format("0.00a")}
+                </td>
+                <td className={`px-4 py-2 ${coin.price_change_percentage_24h >= 0 ? "text-green-400" : "text-red-400"}`}>
+                    {coin.price_change_percentage_24h?.toFixed(2)}%
+                </td>
+            </tr>
+        ))}
+    </tbody>
+</table>
+
+    
         );
     };
         
