152243774c6b6f65499e49f017bcaea7680d04b9
Angel Aquino <aa23-0272@unphu.edu.do>
2025-05-17 00:31:27 -0400

    new with image and new api

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,22 +1,21 @@
 import { useEffect, useState } from "react";
 import Loader from "../components/Loader";
+import toast from "react-hot-toast";
 
 type Currency = {
   code: string;
-  title: string;
-  slug: string;
-  url: string;
 }
 
 type NewsItem = {
   id: number;
   title: string;
   url: string;
-  published_at: string;
+  author?: string;
+  publishedAt: string;
   source: {
-    title: string;
-    domain: string;
+    name: string;
   };
+  urlToImage?: string;
   currencies: Currency[];
 };
 
@@ -27,17 +26,34 @@ export const News = () => {
   const [loading, setLoading] = useState(true);
 
 
-  const fetchNewsCoins = () => {
-    fetch('/api/posts/?auth_token=950119400e3105dc16a70de3c3486c9c26e4da91')
-      .then((response) => response.json())
-      .then((data) => {
-        setNews(data.results);
+  const fetchNewsCoins = async () => {
+    try {
+      const response = await fetch(`https://newsapi.org/v2/everything?q=cryptocurrency&apiKey=${import.meta.env.VITE_NEW_API_KEY}`);
+      const data = await response.json();
+      const formattedNews = data.articles
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          .filter((article: any) => article.urlToImage)
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          .map((article:any, index: number) => ({
+            id: index.toString(),
+            title: article.title,
+            url: article.url,
+            author: article.author,
+            publishedAt: article.publishedAt,
+            source: {
+              name: article.source.name,
+            },
+            urlToImage: article.urlToImage, 
+          }));
+        setNews(formattedNews);
         setLoading(false);
-      })
-      .catch((error) => {
-        console.error('error fetching news:', error);
-        setLoading(false);
-      });
+      
+    } catch (error) {
+      console.log('Error fetching news:', error);
+      setLoading(false);
+      toast.error('Error fetching news', { position: "bottom-right", duration: 2000, style: { background: '#1F2937', color: '#F9FAFB' } });
+      
+    }
   };
 
   useEffect(() => {
@@ -52,6 +68,8 @@ export const News = () => {
     return <Loader/>
   }
 
+  const placeholderImage = 'https://ui.shadcn.com/placeholder.svg';
+
   return (
     <div className="container mx-auto max-w-7xl p-4">
       <h1 className="text-6xl font-bold text-center mb-10">News</h1>
@@ -60,7 +78,14 @@ export const News = () => {
           <div
           key={item.id}
           className="bg-white/5 shadow-md rounded-lg p-4 flex flex-col justify-between border border-white/10 hover:bg-white/10 transition-shadow duration-300"
+          onClick={() => window.open(item.url, "_blank")}
           >
+            <img
+              src={item.urlToImage || placeholderImage} // Usar el placeholder si no hay imagen
+              alt={item.title}
+              className="h-48 w-full object-cover rounded-t-lg mb-4"
+              onError={(e) => { e.currentTarget.src = placeholderImage; }} // Manejo de error
+            />
             <a 
             href={item.url}
             target="_blank"
@@ -71,8 +96,12 @@ export const News = () => {
             </a>
 
             <p className="text-md text-gray-500 mt-2 mb-4">
-              {item.source.title} - {new Date(item.published_at).toLocaleDateString()}
+              {item.source.name} - {new Date(item.publishedAt).toLocaleDateString()}
             </p>
+
+            <span className="text-md text-gray-500 mt-2 mb-4 ">
+            {item.author ? `by ${item.author}` : 'by Unknown'}
+            </span>
 
             <div className="flex flex-wrap gap-2 mt-3">
             {item.currencies?.map((currency) => (
