882f494d40d8bddaf8cff21a61aa642d5df0d925
Angel Aquino <aa23-0272@unphu.edu.do>
2025-05-26 00:27:46 -0400

    add auth section to app

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,11 +1,63 @@
-
+import { useEffect, useState } from 'react';
+import { supabase } from '../supabase/client';
 
 export const Auth = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [session, setSession] = useState<any>(null);
 
-export default Auth
+  useEffect(() => {
+    // Verifica si ya hay sesión activa
+    const getSession = async () => {
+      const { data: { session } } = await supabase.auth.getSession();
+      setSession(session);
+    };
+
+    getSession();
+
+    // Escucha cambios en la sesión
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  const handleLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: 'localhost:5173/auth/callback', // Cambia esto a tu URL de redirección
+      }
+    });
+  };
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+  };
+
+  return (
+    <div className="flex-1 flex justify-end space-x-6">
+      {session ? (
+        <button
+          onClick={handleLogout}
+          className="rounded-full border border-white/10 flex justify-center items-center gap-x-2 py-2 px-4
+          bg-white/5 hover:scale-110 hover:bg-white/10 hover:shadow-lg transition-all duration-300 ease-in-out"
+        >
+          sign out
+        </button>
+      ) : (
+        <button
+          onClick={handleLogin}
+          className="rounded-full border border-white/10 flex justify-center items-center gap-x-2 py-2 px-4
+          bg-white/5 hover:scale-110 hover:bg-white/10 hover:shadow-lg transition-all duration-300 ease-in-out"
+        >
+          sign in
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Auth;
