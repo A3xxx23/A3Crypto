ed83bffc381a182cadbdb0fc7d85b656d8cb545e
Angel Aquino <aa23-0272@unphu.edu.do>
2025-05-26 00:29:22 -0400

    delete console.log

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -3,11 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseURL = import.meta.env.VITE_PROJECT_URL_SUPABASE;
 const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 
-if (!supabaseURL || !supabaseKey) {
-  throw new Error("Supabase URL and API Key must be defined in environment variables.");
-}
-
-console.log("Supabase URL:", supabaseURL);
-console.log("Supabase Key:", supabaseKey);
-
 export const supabase = createClient(supabaseURL, supabaseKey);
